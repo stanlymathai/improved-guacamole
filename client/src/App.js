5b941e654a60e36c86c6467483b573e8e3bd2cdc
stanlymathai <stanly@mathai.com>
2023-09-15 03:29:31 +0530

    chore; add login page ui

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,23 @@
-import './App.css';
+// import Login from './components/auth.component/Login';
+import Register from './components/auth.component/Register';
+
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <h1>React Chat App</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          {/* <ProtectedRoute exact path="/" element={Chat} /> */}
+          {/* <Route path="/login" element={Login} /> */}
+          <Route path="/register" element={<Register />} />
+          <Route render={() => <h1>404 page not found</h1>} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
