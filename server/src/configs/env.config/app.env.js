f320e10acaca5ba4c27628a9acb6647a8dec5633
stanlymathai <stanly@mathai.com>
2023-09-14 22:35:03 +0530

    chore; added server dependencies

---
+++ server/src/configs/env.config/app.env.js
@@ -0,0 +1,6 @@
+module.exports = {
+  app_env: process.env.APP_ENV,
+  app_key: process.env.APP_KEY,
+  app_url: process.env.APP_URL,
+  app_port: process.env.APP_PORT,
+};
---
+++ server/src/configs/env.config/db.env.js
@@ -0,0 +1,8 @@
+module.exports = {
+  db_name: process.env.DB_NAME,
+  db_host: process.env.DB_HOST,
+  db_port: process.env.DB_PORT,
+  db_user: process.env.DB_USER,
+  db_dialect: process.env.DB_DIALECT,
+  db_password: process.env.DB_PASSWORD,
+};
