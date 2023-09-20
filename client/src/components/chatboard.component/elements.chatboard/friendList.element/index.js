d66460eb9f4e732b0a5b724d4a8adb7ed1725087
stanlymathai <stanly@mathai.com>
2023-09-20 15:40:06 +0530

    chore; add 'add chat UI'

--- client/src/components/chatboard.component/elements.chatboard/friendList.element/index.js
+++ client/src/components/chatboard.component/elements.chatboard/friendList.element/index.js
@@ -1,7 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, Fragment } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { setCurrentChat } from '../../../../store/actions.store/chat.action';
 
+import chatService from '../../../../services/chat.service';
+
+import Modal from '../modal.element';
 import Friend from '../friend.element';
 
 import './friendList.scss';
@@ -9,13 +12,22 @@ import './friendList.scss';
 const FriendList = () => {
   const dispatch = useDispatch();
   const chats = useSelector((state) => state.chat.chats);
+  const socket = useSelector((state) => state.chat.socket);
 
   const [showFriendsModal, setShowFriendsModal] = useState(false);
+  const [suggestions, setSuggestions] = useState([]);
 
   // handler fn's
   const openChat = (chat) => {
-    console.log(chat, 'chat open handler');
     dispatch(setCurrentChat(chat));
+  };
+
+  const searchFriends = (e) => {
+    console.log('searching friends', e.target.value);
+  };
+
+  const addNewFriend = (id) => {
+    console.log('adding new friend', id);
   };
 
   return (
@@ -38,6 +50,34 @@ const FriendList = () => {
           <p id="no-chat">No friends added</p>
         )}
       </div>
+      {showFriendsModal && (
+        <Modal click={() => setShowFriendsModal(false)}>
+          <Fragment key="header">
+            <h3 className="m-0">Create new chat</h3>
+          </Fragment>
+
+          <Fragment key="body">
+            <p>Find friends by typing their name bellow</p>
+            <input
+              onInput={(e) => searchFriends(e)}
+              type="text"
+              placeholder="Search..."
+            />
+            <div id="suggestions">
+              {suggestions.map((user) => {
+                return (
+                  <div key={user.id} className="suggestion">
+                    <p className="m-0">
+                      {user.firstName} {user.lastName}
+                    </p>
+                    <button onClick={() => addNewFriend(user.id)}>ADD</button>
+                  </div>
+                );
+              })}
+            </div>
+          </Fragment>
+        </Modal>
+      )}
     </div>
   );
 };
