f65082bc7313e526d0a181651143aaf14b172d16
stanlymathai <stanly@mathai.com>
2023-09-29 18:33:06 +0530

    chore; test fn's for sprinklr webhook

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -3,6 +3,11 @@ const router = require('express').Router();
 router.get('/health', (_, res) => {
   return res.status(200).send('As Strong as an Ox!');
 });
+router.post('/sprinklr', (req, res) => {
+  console.log('req knri', req);
+  // console.log('res knri', res);
+  return res.status(200).json({ message: 'success' });
+});
 
 router.use('/', require('./auth.route'));
 router.use('/users', require('./user.route'));
